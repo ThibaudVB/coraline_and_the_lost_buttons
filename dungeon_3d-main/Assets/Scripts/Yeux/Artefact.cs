31478d2d921e6223a97f26045ae49f9057d7bcc9
ThibaudVB <thibaudvbpro@gmail.com>
2025-03-19 10:57:18 +0100

    Artefact finish

--- dungeon_3d-main/Assets/Scripts/Yeux/Artefact.cs
+++ dungeon_3d-main/Assets/Scripts/Yeux/Artefact.cs
@@ -3,20 +3,23 @@ using UnityEngine;
 public class Artefact : MonoBehaviour
 {
     public static int artefactsCollected = 0;  // Compteur d'artefacts collectés
+    private bool isCollected = false;          // Flag pour vérifier si l'artefact a déjà été collecté
 
     public static event System.Action<int> OnArtefactCollected; // Événement pour notifier la collecte d'artefacts
 
     // Lorsque le joueur entre en collision avec l'artefact
     void OnTriggerEnter(Collider col)
     {
-        if (col.CompareTag("Player"))  // Si le joueur entre en collision avec l'artefact
+        if (col.CompareTag("Player") && !isCollected)  // Vérifie si c'est le joueur et si l'artefact n'a pas déjà été collecté
         {
-            artefactsCollected++;  // Augmenter le compteur d'artefacts collectés
+            isCollected = true;  // Marque l'artefact comme collecté
+
+            artefactsCollected++;  // Augmente le compteur d'artefacts collectés
 
             // Déclenche l'événement pour notifier les autres scripts
             OnArtefactCollected?.Invoke(artefactsCollected);
 
-            Destroy(gameObject);  // Supprimer l'artefact après sa collecte
+            Destroy(gameObject);  // Supprime l'artefact après sa collecte
         }
     }
 }
--- dungeon_3d-main/Assets/Scripts/Yeux/DoorManager.cs
+++ dungeon_3d-main/Assets/Scripts/Yeux/DoorManager.cs
@@ -4,32 +4,10 @@ public class DoorManager : MonoBehaviour
 {
     public GameObject door;  // La porte à déverrouiller
 
-    // Méthode pour déverrouiller la porte
+    // Déverrouille la porte
     void UnlockDoor()
     {
-        // Désactiver la porte pour la déverrouiller
-        door.SetActive(false);  // Cela désactive la porte dans la scène
+        door.SetActive(true);  // La porte devient visible
         Debug.Log("Porte déverrouillée !");
-    }
-
-    void OnEnable()
-    {
-        // Abonnement à l'événement OnArtefactCollected
-        Artefact.OnArtefactCollected += CheckArtefactsForUnlock;
-    }
-
-    void OnDisable()
-    {
-        // Désabonnement de l'événement lors de la destruction du script ou du changement d'objet
-        Artefact.OnArtefactCollected -= CheckArtefactsForUnlock;
-    }
-
-    // Vérifie si le joueur a collecté tous les artefacts et déverrouille la porte
-    void CheckArtefactsForUnlock(int artefactsCount)
-    {
-        if (artefactsCount >= 10)
-        {
-            UnlockDoor();  // Appelle la méthode pour déverrouiller la porte
-        }
     }
 }
